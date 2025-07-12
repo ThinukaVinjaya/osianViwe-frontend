4714204ab7cec9844dc90757285257e029d7de41
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2025-07-12 21:30:38 +0530

    add admin pannel and nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,28 @@ import ExistingRoom from './components/room/ExistingRoom.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import EditRoom from './components/room/EditRoom.jsx'
 import Home from './components/home/Home.jsx'
+import NavBar from './components/layout/NavBar.jsx'
+import Footer from './components/layout/Footer.jsx'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import RoomListing from './components/room/RoomListing.jsx'
+import Admin from './components/admin/Admin.jsx'
 
 function App() {
   return (
     <>
     <main>
       <Router>
+        <NavBar/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/edit-room/:roomId" element={<EditRoom/>} />
           <Route path="/existing-rooms" element={<ExistingRoom/>} />
           <Route path="/add-room" element={<AddRoom/>} />
+          <Route path="/browse-all-rooms" element={<RoomListing/>} />
+          <Route path="/admin" element={<Admin/>} />
         </Routes>
       </Router>
+      <Footer/>
     </main>
     </>
   )
