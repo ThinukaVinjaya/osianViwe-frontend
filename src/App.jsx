8175001d5daf52f0b014fb98db6947fc3ae41c70
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2025-07-16 08:54:04 +0530

    bookingform

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import Footer from './components/layout/Footer.jsx'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import RoomListing from './components/room/RoomListing.jsx'
 import Admin from './components/admin/Admin.jsx'
+import Checkout from './components/bookings/Checkout.jsx'
+import BookingSuccess from './components/bookings/BookingSuccess.jsx'
 
 function App() {
   return (
@@ -26,8 +28,10 @@ function App() {
           <Route path="/edit-room/:roomId" element={<EditRoom/>} />
           <Route path="/existing-rooms" element={<ExistingRoom/>} />
           <Route path="/add-room" element={<AddRoom/>} />
+          <Route path="/book-room/:roomId" element={<Checkout/>} />
           <Route path="/browse-all-rooms" element={<RoomListing/>} />
           <Route path="/admin" element={<Admin/>} />
+          <Route path="/booking-success" element={<BookingSuccess/>} />
         </Routes>
       </Router>
       <Footer/>
