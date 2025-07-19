a97e744bced1b0df673d00320da54f77cf5cd4f2
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2025-07-19 05:37:01 +0530

    make bookingsTable

---
+++ src/components/common/DateSlider.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "react-date-range/dist/styles.css"
+import "react-date-range/dist/theme/default.css"
+import { DateRangePicker } from "react-date-range";
+
+
+
+const DateSlider = ({ onDateChange, onFilterChange }) => {
+    const [dateRange, setDateRange] = useState({
+        startDate: undefined,
+        sendDate: undefined,
+        key: "selection"
+    })
+
+    const handleSelect = (ranges) => {
+        setDateRange(ranges.selection)
+        onDateChange(ranges.selection.startDate, ranges.selection.sendDate)
+        onFilterChange(ranges.selection.startDate, ranges.selection.sendDate)
+    }
+
+    const handleClearFilter = () => {
+        setDateRange({
+            startDateT: undefined,
+            sendDate: undefined,
+            key: "selection"
+        })
+        onDateChange(null, null)
+        onFilterChange(null, null)
+    }
+    return (
+        <>
+        <h5>Filter bookings by date</h5>
+        <DateRangePicker ranges={[dateRange]} onChange={handleSelect} className="mb-4"/>
+        <button className="btn-secondary" onClick={handleClearFilter}>
+            Clear Filter
+        </button>
+
+        </>
+    )
+}
+
+export default DateSlider
